b972240933776c2585977afab0ff913218a17076
Jaikar Beasley <jaikarbeasley@gmail.com>
2023-10-02 19:56:55 -0700

    install and screens
    expo install and screens added

---
+++ screens/AllPlaces.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function AllPlaces() {
+  return (
+    <View>
+      <Text>AllPlaces</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
