14fac2e0bb32244e96fbf11e4dc82ad5ab58763e
Jaikar Beasley <jaikarbeasley@gmail.com>
2023-10-05 00:11:42 -0700

    added map screen and map package

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import IconButton from './components/UI/IconButton';
 import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
+import Map from './screens/Map';
 import { Colors } from './constants/colors';
 
 const Stack = createNativeStackNavigator();
@@ -43,6 +44,7 @@ export default function App() {
               title: 'Add a new place',
             }}
           />
+          <Stack.Screen name="Map" component={Map} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
